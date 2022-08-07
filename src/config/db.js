66ec8f3b9bc015496e380523dbad46850f3a209d
isakkasi <83792153+isakkasi@users.noreply.github.com>
2022-08-07 08:58:21 +0300

    DB path change for deployment

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
-// exports.URI = 'mongodb+srv://qdb:Amentia6J3@qdb.kxljedn.mongodb.net/?retryWrites=true&w=majority'
-exports.URI = 'mongodb://localhost:27017/exam-question'
+exports.URI = 'mongodb+srv://qdb:Amentia6J3@qdb.kxljedn.mongodb.net/?retryWrites=true&w=majority'
+// exports.URI = 'mongodb://localhost:27017/exam-question'
 
 // exports.DB = 'project-a'
 // exports.DB = 'project-a'
