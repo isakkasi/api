5c93b92df6ed9898eeeaa366463ce88510347faa
isakkasi <83792153+isakkasi@users.noreply.github.com>
2022-10-17 22:27:28 +0300

    DB Settings

--- src/config/db.js
+++ src/config/db.js
@@ -2,5 +2,5 @@ exports.URI = 'mongodb+srv://qdb:Amentia6J3@qdb.kxljedn.mongodb.net/?retryWrites
 // exports.URI = 'mongodb://localhost:27017/exam-question'
 
 // exports.DB = 'project-a'
-// exports.DB = 'project-a'
-exports.DB = 'exam-question'
+exports.DB = 'project-a'
+// exports.DB = 'exam-question'
