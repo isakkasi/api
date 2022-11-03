0ecb7f79ec1e44719233e06dc4e31fa103f130e7
isakkasi <83792153+isakkasi@users.noreply.github.com>
2022-11-03 15:26:00 +0200

    DB config to PROD

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,12 @@
 // PRODUCTION
 
-// exports.URI = 'mongodb+srv://qdb:Amentia6J3@qdb.kxljedn.mongodb.net/?retryWrites=true&w=majority'
-// exports.DB = 'project-a'
+exports.URI = 'mongodb+srv://qdb:Amentia6J3@qdb.kxljedn.mongodb.net/?retryWrites=true&w=majority'
+exports.DB = 'project-a'
 
 
 
 
 // DEVELOPMENT
 
-exports.URI = 'mongodb://localhost:27017/exam-question'
-exports.DB = 'exam-question'
+// exports.URI = 'mongodb://localhost:27017/exam-question'
+// exports.DB = 'exam-question'
