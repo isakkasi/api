87f28f56b67f1a6c37fafa714678825ba6032f81
isakkasi <83792153+isakkasi@users.noreply.github.com>
2022-11-03 15:25:27 +0200

    Db config change

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,12 @@
-exports.URI = 'mongodb+srv://qdb:Amentia6J3@qdb.kxljedn.mongodb.net/?retryWrites=true&w=majority'
-// exports.URI = 'mongodb://localhost:27017/exam-question'
+// PRODUCTION
 
+// exports.URI = 'mongodb+srv://qdb:Amentia6J3@qdb.kxljedn.mongodb.net/?retryWrites=true&w=majority'
 // exports.DB = 'project-a'
-exports.DB = 'project-a'
-// exports.DB = 'exam-question'
+
+
+
+
+// DEVELOPMENT
+
+exports.URI = 'mongodb://localhost:27017/exam-question'
+exports.DB = 'exam-question'
