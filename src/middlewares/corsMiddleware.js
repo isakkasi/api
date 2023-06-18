39176be0474f43f2f78e8deed49f532343b93f75
isakkasi <83792153+isakkasi@users.noreply.github.com>
2023-06-19 00:57:50 +0300

    Package update and CORS

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -3,7 +3,7 @@
 exports.cors = () => (req, res, next) => {
 
     //set the ip address of the app
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://qdb.pc-acad.com/');
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
